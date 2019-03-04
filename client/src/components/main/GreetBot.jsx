fcf865af467b00055f304a590c21e083f5eaf525
jmohon1986 <jmohon1986@gmail.com>
2019-03-04 01:10:48 -0600

    add working options for chatbot remove voice toggle bug

--- client/src/components/main/GreetBot.jsx
+++ client/src/components/main/GreetBot.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+// import Axios from 'axios';
+import jokes from '../../jokes.js';
+import compliments from '../../compliments.js';
 import ChatBot from '../../../react-simple-chatbot';
 import Credentials from '../login/Credentials.jsx';
 
@@ -10,8 +13,30 @@ class GreetForm extends React.Component {
       favFood: '',
       opened: true,
     };
+    this.count = 0;
+
+    this.compliment = () => {
+      const comp = compliments[this.count];
+      this.count += 1;
+      if (this.count === compliments.length) {
+        this.count = 0;
+      }
+      return comp;
+    };
+
+    this.joke = () => {
+      const joke = jokes[this.count];
+      this.count += 1;
+      if (this.count === jokes.length) {
+        this.count = 0;
+      }
+      return joke;
+    };
   }
 
+  componentDidMount() {
+    this.compliment();
+  }
 
   toggleFloating({ opened }) {
     this.setState({ opened });
@@ -37,7 +62,7 @@ class GreetForm extends React.Component {
           steps={[
             {
               id: '1',
-              message: `Hello ${user}, I am 'CAIN', (Client. Appetite. Indulgence. Network).`,
+              message: `Hello ${user}, I am 'CAIN'. The Client, Appetite, Indulgence, Network.`,
               trigger: '3',
             },
             {
@@ -82,7 +107,7 @@ class GreetForm extends React.Component {
             },
             {
               id: '6',
-              message: 'Fuck yeah, {previousValue}!!!',
+              message: 'Yeah it is!',
               trigger: '7',
             },
             {
@@ -112,17 +137,66 @@ class GreetForm extends React.Component {
             {
               id: 'update-allergies',
               update: 'allergies',
-              trigger: 'allergies',
+              trigger: '3',
             },
             {
               id: 'update-favFood',
               update: 'favFood',
-              trigger: 'favFood',
+              trigger: '5',
             },
             {
               id: 'end-message',
-              message: 'Thanks! Your data was submitted successfully!',
+              message: 'Thanks! Your data was submitted successfully! For a short list of other commands, type help or click the send button!',
+              trigger: '9',
+            },
+            {
+              id: '9',
+              message: 'Okay! How can I help?',
+              user: true,
+              trigger: 'a',
+            },
+            {
+              id: 'a',
+              options: [
+                { value: 'Switch Account/Create Account', label: 'Switch Account\nor\nCreate Account', trigger: 'b' },
+                { value: 'Logout', label: 'Log me out', trigger: 'c' },
+                { value: 'Give me a compliment', label: 'Give me a compliment', trigger: 'd' },
+                { value: 'Tell me a joke', label: 'Tell me a joke', trigger: 'e' },
+              ],
+            },
+            {
+              id: 'b',
+              component: (
+                <Credentials
+                  signUp={window.signup}
+                  login={window.login}
+                  guestLogin={window.guest}
+                />
+              ),
+              trigger: 'a',
+            },
+            {
+              id: 'c',
+              message: 'Please click to confirm logout.',
+              trigger: 'logout',
+            },
+            {
+              id: 'logout',
+              user: true,
+              validator: () => {
+                window.logout();
+              },
               end: true,
+            },
+            {
+              id: 'd',
+              message: this.compliment,
+              trigger: 'a',
+            },
+            {
+              id: 'e',
+              message: this.joke,
+              trigger: 'a',
             },
           ]}
           floating
@@ -169,12 +243,12 @@ class GreetForm extends React.Component {
               id: '5',
               component: (
                 <Credentials
-                signUp={window.signup}
-                login={window.login}
-                guestLogin={window.guest}
-              />
+                  signUp={window.signup}
+                  login={window.login}
+                  guestLogin={window.guest}
+                />
               ),
-              trigger: '2',
+              trigger: '4',
             },
             {
               id: '6',
@@ -191,43 +265,13 @@ class GreetForm extends React.Component {
             },
             {
               id: '7',
-              options: [
-                { value: 'yes', label: 'Yes', trigger: 'update-yes' },
-                { value: 'no', label: 'No', trigger: 'end-message' },
-              ],
-            },
-            {
-              id: 'update-yes',
-              message: 'What field would you like to update?',
-              trigger: '8',
+              message: this.compliment,
+              trigger: '4',
             },
             {
               id: '8',
-              options: [
-                { value: 'name', label: 'Name', trigger: 'update-name' },
-                { value: 'allergies', label: 'allergies', trigger: 'update-allergies' },
-                { value: 'favFoods', label: 'Favorite Food', trigger: 'update-favFood' },
-              ],
-            },
-            {
-              id: 'update-name',
-              update: 'name',
-              trigger: '7',
-            },
-            {
-              id: 'update-allergies',
-              update: 'allergies',
-              trigger: '7',
-            },
-            {
-              id: 'update-favFood',
-              update: 'favFood',
-              trigger: '7',
-            },
-            {
-              id: 'end-message',
-              message: 'Thanks! Your data was submitted successfully!',
-              end: true,
+              message: this.joke,
+              trigger: '4',
             },
           ]}
           floating
@@ -247,11 +291,17 @@ class GreetForm extends React.Component {
               id: '1',
               message: `Hello ${user}!, I am 'CAIN'. 
               The "Client, Appitite, Indulgence, Network".`,
+              trigger: '2',
+            },
+            {
+              id: '2',
+              message: 'To get started, begin typing the ingredients you have on hand, or want to avoid, in the search field. After you are satisfied with your ingredients, just click the "Search for a recipe" button and you will be presented with a custom recipe list, tailored to your specifications. Type help for more options.',
               trigger: '3',
             },
             {
               id: '3',
-              message: 'To get started, begin typing the ingredients you have on hand, or want to avoid, in the search field. After you are satisfied with your ingredients, just click the "Search for a recipe" button and you will be presented with a custom recipe list, tailored to your specifications',
+              message: 'Okay! How can I help?',
+              user: true,
               trigger: '4',
             },
             {
@@ -272,7 +322,7 @@ class GreetForm extends React.Component {
                   guestLogin={window.guest}
                 />
               ),
-              trigger: '2',
+              trigger: '4',
             },
             {
               id: '6',
@@ -289,48 +339,13 @@ class GreetForm extends React.Component {
             },
             {
               id: '7',
-              message: 'Would you like to update any of the previous fields?',
-              trigger: 'update-question',
-            },
-            {
-              id: 'update-question',
-              options: [
-                { value: 'yes', label: 'Yes', trigger: 'update-yes' },
-                { value: 'no', label: 'No', trigger: 'end-message' },
-              ],
-            },
-            {
-              id: 'update-yes',
-              message: 'What field would you like to update?',
-              trigger: '8',
+              message: this.compliment,
+              trigger: '4',
             },
             {
               id: '8',
-              options: [
-                { value: 'name', label: 'Name', trigger: 'update-name' },
-                { value: 'allergies', label: 'allergies', trigger: 'update-allergies' },
-                { value: 'favFoods', label: 'Favorite Food', trigger: 'update-favFood' },
-              ],
-            },
-            {
-              id: 'update-name',
-              update: 'name',
-              trigger: '7',
-            },
-            {
-              id: 'update-allergies',
-              update: 'allergies',
-              trigger: '7',
-            },
-            {
-              id: 'update-favFood',
-              update: 'favFood',
-              trigger: '7',
-            },
-            {
-              id: 'end-message',
-              message: 'Thanks! Your data was submitted successfully!',
-              end: true,
+              message: this.joke,
+              trigger: '4',
             },
           ]}
           floating
@@ -373,69 +388,30 @@ class GreetForm extends React.Component {
                 guestLogin={window.guest}
               />
             ),
-            trigger: 'favFood',
-          },
-          {
-            id: 'favFood',
-            user: true,
-            validator: (value) => {
-              if (value) {
-                this.setState({ favFood: value });
-                return true;
-              }
-              return true;
-            },
-            trigger: '6',
+            trigger: '4',
           },
           {
             id: '6',
-            message: 'Fuck yeah, {previousValue}!!!',
-            trigger: '7',
+            message: 'Please click to confirm logout.',
+            trigger: 'logout',
+          },
+          {
+            id: 'logout',
+            user: true,
+            validator: () => {
+              window.logout();
+            },
+            end: true,
           },
           {
             id: '7',
-            message: 'Would you like to update any of the previous fields?',
-            trigger: 'update-question',
-          },
-          {
-            id: 'update-question',
-            options: [
-              { value: 'yes', label: 'Yes', trigger: 'update-yes' },
-              { value: 'no', label: 'No', trigger: 'end-message' },
-            ],
-          },
-          {
-            id: 'update-yes',
-            message: 'What field would you like to update?',
-            trigger: '8',
+            message: this.compliment,
+            trigger: '4',
           },
           {
             id: '8',
-            options: [
-              { value: 'name', label: 'Name', trigger: 'update-name' },
-              { value: 'allergies', label: 'allergies', trigger: 'update-allergies' },
-              { value: 'favFoods', label: 'Favorite Food', trigger: 'update-favFood' },
-            ],
-          },
-          {
-            id: 'update-name',
-            update: 'name',
-            trigger: '7',
-          },
-          {
-            id: 'update-allergies',
-            update: 'allergies',
-            trigger: '7',
-          },
-          {
-            id: 'update-favFood',
-            update: 'favFood',
-            trigger: '7',
-          },
-          {
-            id: 'end-message',
-            message: 'Thanks! Your data was submitted successfully!',
-            end: true,
+            message: this.joke,
+            trigger: '4',
           },
         ]}
         floating
---
+++ client/src/jokes.js
@@ -0,0 +1,25 @@
+const jokes = [
+  `What did one blueberry say to the other blueberry?
+  If you weren’t so sweet, we wouldn’t be in this jam.`,
+  `My friend thinks he is so smart. He told me an onion is the only food that makes you cry,
+  so I threw a coconut at his face.`,
+  `Why do hamburgers go to the gym? 
+  To get better buns!`,
+  `How much room is needed for fungi to grow?
+  As mushroom as possible`,
+  `If you have 13 apples in one hand and 10 oranges in the other, we can assume that you have 
+  freakishly large hands.`,
+  `A lot of people cry when they cut an onion.
+  The trick is not to form an emotional bond.`,
+  `There were two cannibals who captured a man. 
+  They decided it would be fair if they started eating from opposite ends. 
+  After a few minutes, the one who started at the head asked the other one, 
+  "How's it going down there?" And the other one replies, 
+  "I'm having a ball!"`,
+  `How can you drop a raw egg onto a concrete floor without cracking it? 
+  Any way you want, concrete floors are very hard to crack.`,
+  `I told my friend I was going to make a car out of spaghetti. 
+  She couldn't believe it when I drove pasta.`,
+];
+
+export default jokes;
